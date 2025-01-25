41b7c7c15679ebee772a549920fd761d5c326f12
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2025-01-25 14:20:45 -0500

    Initial tweaks of code to run stand-alone

---
+++ v1/All stats code 01152025.R
@@ -0,0 +1,36 @@
+#ER
+library(readxl)
+
+data_frame<-read_excel("ERforBoxplot(no blanks)07102024.xlsx")
+table(data_frame$`cohort`, data_frame$`CategoriesAdd7102024`)
+
+fisher.test(data_frame$`cohort`, data_frame$`CategoriesAdd7102024`)
+
+#PR
+library(readxl)
+
+data_frame<-read_excel("PRforBoxplot(no blanks)07112024.xlsx")
+table(data_frame$`cohort`, data_frame$`CategoriesAdd08222024`)
+
+chisq.test(data_frame$`cohort`, data_frame$`CategoriesAdd08222024`)
+
+#HER2
+library(readxl)
+
+data_frame<-read_excel("HER2forBarChart(no blanks)07112024.xlsx")
+
+table(data_frame$cohort,data_frame$`Category`)
+
+chisq.test(data_frame$cohort,data_frame$`Category`, correct=FALSE)
+
+#HR/HER2
+library(readxl)
+data_frame<-(read_excel("HRHER2forR(no blanks) MR 09102024.xlsx"))
+ft<-table(data_frame$`cohort`,data_frame$`HRHER2_MR`)
+table(data_frame$`cohort`,data_frame$`HRHER2_MR`)
+
+fisher.test(ft, workspace=2e6)
+
+#Ki-67 (included in other file)
+
+#PAM50
