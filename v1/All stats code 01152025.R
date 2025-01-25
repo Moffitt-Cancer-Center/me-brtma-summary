028f05c53daf6aaad1ff53787c73a588e15f5c89
Steven Eschrich <35569991+steveneschrich@users.noreply.github.com>
2025-01-25 16:09:57 -0500

    Patched script to read from v1 directory.

--- v1/All stats code 01152025.R
+++ v1/All stats code 01152025.R
@@ -1,7 +1,7 @@
 #ER
 library(readxl)
 
-data_frame<-read_excel("ERforBoxplot(no blanks)07102024.xlsx")
+data_frame<-read_excel(here::here("v1/ERforBoxplot(no blanks)07102024.xlsx"))
 table(data_frame$`cohort`, data_frame$`CategoriesAdd7102024`)
 
 fisher.test(data_frame$`cohort`, data_frame$`CategoriesAdd7102024`)
@@ -9,7 +9,7 @@ fisher.test(data_frame$`cohort`, data_frame$`CategoriesAdd7102024`)
 #PR
 library(readxl)
 
-data_frame<-read_excel("PRforBoxplot(no blanks)07112024.xlsx")
+data_frame<-read_excel(here::here("v1/PRforBoxplot(no blanks)07112024.xlsx"))
 table(data_frame$`cohort`, data_frame$`CategoriesAdd08222024`)
 
 chisq.test(data_frame$`cohort`, data_frame$`CategoriesAdd08222024`)
@@ -17,7 +17,7 @@ chisq.test(data_frame$`cohort`, data_frame$`CategoriesAdd08222024`)
 #HER2
 library(readxl)
 
-data_frame<-read_excel("HER2forBarChart(no blanks)07112024.xlsx")
+data_frame<-read_excel(here::here("v1/HER2forBarChart(no blanks)07112024.xlsx"))
 
 table(data_frame$cohort,data_frame$`Category`)
 
@@ -25,7 +25,7 @@ chisq.test(data_frame$cohort,data_frame$`Category`, correct=FALSE)
 
 #HR/HER2
 library(readxl)
-data_frame<-(read_excel("HRHER2forR(no blanks) MR 09102024.xlsx"))
+data_frame<-(read_excel(here::here("v1/HRHER2forR(no blanks) MR 09102024.xlsx")))
 ft<-table(data_frame$`cohort`,data_frame$`HRHER2_MR`)
 table(data_frame$`cohort`,data_frame$`HRHER2_MR`)
 
